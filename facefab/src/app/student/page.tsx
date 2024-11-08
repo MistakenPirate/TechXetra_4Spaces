4c025cfe4db57c8b03b556fbbd0160d29c2a0adb
Ash-art-arch <ashishsaikia092@gmail.com>
2024-11-08 16:40:46 +0530

    fix:background

--- facefab/src/app/student/page.tsx
+++ facefab/src/app/student/page.tsx
@@ -2,9 +2,9 @@ import pinkBlob from '../../../public/blob.svg'
 import Image from 'next/image';
 export default function Student(){
     return(
-        <div className="bg-[#020722] w-full overflow-x-hidden">
-           <section className="w-full ">
-            <Image src={pinkBlob} className="absolute right-0 top-0" alt="blob" />
+        <div className="bg-[#020722] w-full overflow-x-hidden ">
+           <section className="w-full bg-[#020722]  ">
+            <Image src={pinkBlob} className="absolute right-0 top-0 w-[20%]" alt="blob" />
             
            </section>
         </div>
