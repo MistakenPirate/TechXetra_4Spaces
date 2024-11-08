28435e7f2a3542ada5aaf75186ffe635825aa192
Ash-art-arch <ashishsaikia092@gmail.com>
2024-11-08 16:30:20 +0530

    feature:student

--- facefab/src/app/student/page.tsx
+++ facefab/src/app/student/page.tsx
@@ -1,7 +1,12 @@
+import pinkBlob from '../../../public/blob.svg'
+import Image from 'next/image';
 export default function Student(){
     return(
-        <div>
-            Hello World;
+        <div className="bg-[#020722] w-full overflow-x-hidden">
+           <section className="w-full ">
+            <Image src={pinkBlob} className="absolute right-0 top-0" alt="blob" />
+            
+           </section>
         </div>
     );
 }
