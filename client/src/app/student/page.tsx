c49aebc8388314975425b1e81d5116a39d6ae6cb
Ash-art-arch <ashishsaikia092@gmail.com>
2024-11-09 12:31:07 +0530

    feature:faculty page

--- client/src/app/student/page.tsx
+++ client/src/app/student/page.tsx
@@ -6,7 +6,7 @@ export default function StudentDashboard(){
     return(
         <div className=" w-full min-h-[calc(100vh-4.5rem)] overflow-x-hidden ">
            <section className="w-full h-full  px-10 py-10 ">
-            <Image src={pinkBlob} className="absolute right-0 top-0 w-[20%]" alt="blob" />
+            <Image src={pinkBlob} className="absolute right-0 top-0 w-[20%] z-[-10]" alt="blob" />
             <Image src={orangeBlob} className="absolute left-0 bottom-0 w-[20%]" alt="blob" />
             <Cards/>
            </section>
--- client/src/components/ui/cards.tsx
+++ client/src/components/ui/cards.tsx
@@ -6,7 +6,7 @@ export default function Cards() {
            <h1 className="font-bold text-2xl tracking-widest text-white leading-10 montserrat">        
                TOC
             </h1>
-            <CircleProgressBar percentage={70} circleWidth={200}/>
+            <CircleProgressBar percentage={90} circleWidth={200}/>
             <p className="space-y-2">You can miss 3 more class</p>
         </div>
     );
